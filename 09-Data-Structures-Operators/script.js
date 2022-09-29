af4ea899b82f71ba1c327facf022eedad6838e2b
Cristian Rodriguez <an1me@hotmail.es>
2022-09-29 18:55:19 -0500

    operadores de asignacion

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -53,6 +53,36 @@ const restaurant = {
   },
 };
 
+const rest1  = {
+  name: 'Capri',
+  numGuests: 0,
+}
+
+const rest2 = {
+  name: 'La piazza',
+  owner: 'Cristian Rodriguez',
+}
+/* 
+OR opereador asisgancion
+rest1.numGuests = rest1.numGuests || 10;
+rest2.numGuests = rest2.numGuests || 10;
+ 
+rest1.numGuests ||= 10;
+rest2.numGuests ||= 10;
+*/
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+/* rest1.owner = rest1.owner && '<ANONYMOUS>';
+rest2.owner = rest2.owner && '<ANONYMOUS>'; */
+
+// AND assignment operator
+rest1.owner &&= '<ANONYMOUS>'
+rest2.owner &&= '<ANONYMOUS>'
+
+console.log(rest1);
+console.log(rest2);
+
 /* 
 //! rest funcion ordenPizza
 restaurant.ordenPizza('chicharron', 'queso', 'arina', 'carne', 'cebolla');
