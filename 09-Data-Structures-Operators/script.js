e4c60868e0a7986dc22c2f1de328aab9cf70e001
Cristian Rodriguez <an1me@hotmail.es>
2022-10-22 11:06:57 -0500

    data structura

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -53,6 +53,52 @@ const restaurant = {
   },
 };
 
+//const airLine = "TAP Air Portugal";
+//const plane = "A320";
+
+function filterArray(numbers, value) {
+  // Change code below this line
+  let newArray = [];
+  for (let i = 0; i < numbers.length; i += 1) {
+    // Change this line
+    if (numbers[i] > value) {
+      newArray.push(numbers[i]);
+    }
+  }
+  return newArray;
+  // Change code above this line
+}
+console.log(filterArray([1, 2, 3, 4, 5], 3));
+console.log(filterArray([1, 2, 3, 4, 5], 4));
+console.log(filterArray([1, 2, 3, 4, 5], 5));
+console.log(filterArray([12, 24, 8, 41, 76], 38));
+
+
+// console.log(airLine.indexOf('r'));
+// console.log(airLine.lastIndexOf('r'));
+// console.log(airLine.indexOf('Portugal'));
+
+//Metodo de Corte
+//console.log(airLine.slice(4));
+//console.log(airLine.slice(4, 7));
+
+//console.log(airLine.slice(0, airLine.indexOf(' ')));
+//console.log(airLine.slice(airLine.lastIndexOf(' ') + 1));
+
+//console.log(airLine.slice(-2));
+//console.log(airLine.slice(1, -1));
+/*
+
+const checkMiddleSeat = function (seat) {
+  const s = seat.slice(-1);
+  if (s === 'B' || s === 'E') console.log('Tienes el asiento del medio (●\'◡\'●)');
+  else console.log('Has tenido suerte ヾ(⌐■_■)ノ♪');
+}
+
+checkMiddleSeat('11B');
+checkMiddleSeat('23C');
+checkMiddleSeat('3E');
+*/
 
 
 /* 
